f5b9d1512681fb62d95efa285308965c1c0e43f7
Chris Cardle <cdcardle@gmail.com>
2018-05-23 17:04:59 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'admin/dashboard'
-
-  get 'admin/stats'
-
-  get 'admin/financials'
-
-  get 'admin/settings'
-
   get 'categories/show'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
